25471b86a63a0070dc1a8305f401386635bd8281
Reetika Roy <royritu21@yahoo.co.in>
2014-07-19 22:52:45 +0530

    blah
    stupid stuff

--- lib/connection.php
+++ lib/connection.php
@@ -1,17 +1,17 @@
 <?php
 
- define( "DB_SERVER",    $_ENV['OPENSHIFT_MYSQL_DB_HOST'] );
+ //define( "DB_SERVER",    $_ENV['OPENSHIFT_MYSQL_DB_HOST'] );
 
-   define( "DB_USER",      $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] );
+   // define( "DB_USER",      $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] );
 
-   define( "DB_PASSWORD",  $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] );
+    //define( "DB_PASSWORD",  $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] );
 
-    define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
+    //define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
 
-/*$mysqli = new mysqli("localhost", "root", "", "leprosy");
+$mysqli = new mysqli("localhost", "root", "", "leprosy");
 if (mysqli_connect_errno())
 {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-}*/
+}
 ?>
