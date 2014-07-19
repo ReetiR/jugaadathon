181c2f73c21a7a0ba2ffefdf9ca9df7404877fdb
Reetika Roy <royritu21@yahoo.co.in>
2014-07-19 23:08:56 +0530

    now i am pissed
    >.<

--- lib/connection.php
+++ lib/connection.php
@@ -1,20 +1,20 @@
 <?php
 
- //define( "DB_SERVER",    $_ENV['OPENSHIFT_MYSQL_DB_HOST'] );
+ define( "DB_SERVER",    $_ENV['OPENSHIFT_MYSQL_DB_HOST'] );
 
-   // define( "DB_USER",      $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] );
+  define( "DB_USER",      $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] );
 
-    //define( "DB_PASSWORD",  $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] );
+  define( "DB_PASSWORD",  $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] );
 
-    //define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
+  define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
 
- $hostname = "111.93.137.133";
+ /*$hostname = "111.93.137.133";
   $dbusername = "adminZueeJPa";
   $dbname  = "leprosy";
   $dbpassword = "CaH57HtycZYY";
+*/
 
-
-  $mysqli = new mysqli($hostname, $dbusername, $dbpassword, $dbname);
+  $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
   if (mysqli_connect_errno())
   {
     printf("Connect failed: %s\n", mysqli_connect_error());
