bd7cfa1d27cb1963ffb295fea22a63ed91871da6
Reetika Roy <royritu21@yahoo.co.in>
2014-07-19 22:39:38 +0530

    connection
    blah

--- lib/connection.php
+++ lib/connection.php
@@ -1,4 +1,13 @@
 <?php
+
+ //define( "DB_SERVER",    $_ENV['OPENSHIFT_MYSQL_DB_HOST'] );
+
+   // define( "DB_USER",      $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'] );
+
+    //define( "DB_PASSWORD",  $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'] );
+
+    //define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
+
 $mysqli = new mysqli("localhost", "root", "", "leprosy");
 if (mysqli_connect_errno())
 {
