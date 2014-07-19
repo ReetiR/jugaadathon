3436b5334be41a612df668ca4329546d74c6d4ec
Reetika Roy <royritu21@yahoo.co.in>
2014-07-19 22:54:37 +0530

    -.-
    blah

--- lib/connection.php
+++ lib/connection.php
@@ -8,10 +8,19 @@
 
     //define( "DB_DATABASE",  $_ENV['OPENSHIFT_APP_NAME'] );
 
-$mysqli = new mysqli("localhost", "root", "", "leprosy");
-if (mysqli_connect_errno())
-{
+ $hostname = "localhost";
+  $dbusername = "root";
+  $dbname  = "leprosy";
+  $dbpassword = "";
+
+
+  $mysqli = new mysqli($hostname, $dbusername, $dbpassword, $dbname);
+  if (mysqli_connect_errno())
+  {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-}
+  }
+  if(!isset($_SESSION))
+	   session_start();
+
 ?>
